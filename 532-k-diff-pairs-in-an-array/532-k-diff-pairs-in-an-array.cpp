0e77f858e9606d3db9c0d2b99bca4da771d4afbd
Shivanshu Sanjeev <55157249+Shivanshwho@users.noreply.github.com>
2022-08-11 00:17:13 +0530

    Time: 11 ms (98.06%), Space: 11.2 MB (92.86%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int findPairs(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int ans=0;
-        if(k==0)
+        if(false)
         {
             map<int,int>mp;
             for(int i=0;i<nums.size();i++)
@@ -26,7 +26,7 @@ public:
               continue;
              }
             else{
-                if(binary_search(nums.begin(),nums.end(),nums[i]+k))
+                if(binary_search(nums.begin()+i+1,nums.end(),nums[i]+k))
                 {
                      ans++;
                  }
