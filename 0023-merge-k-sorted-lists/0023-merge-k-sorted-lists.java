03c6d7021799d1bea4c06bb4da503ea56b96bebf
Shivanshu Sanjeev <55157249+Shivanshwho@users.noreply.github.com>
2023-09-16 12:07:31 +0530

    Time: 4 ms (74.98%), Space: 42.8 MB (98.22%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -5,28 +5,33 @@ class Solution {
         // Create a priority queue (min-heap) with a custom comparator
         PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        // Add the first node from each list to the min-heap
+        // Step 1: Add the first node from each list to the min-heap
         for (ListNode list : lists) {
             if (list != null) {
                 minHeap.offer(list);
             }
         }
 
-        // Create a dummy node to simplify the code
+        // Step 2: Create a dummy node to simplify the code
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
 
-        // Merge nodes from the min-heap until it's empty
+        // Step 3: Merge nodes from the min-heap until it's empty
         while (!minHeap.isEmpty()) {
+            // Get the node with the smallest value from the min-heap
             ListNode node = minHeap.poll();
+
+            // Add this node to the merged list
             current.next = node;
             current = current.next;
 
+            // Step 4: If there's a next node in the same list, add it to the min-heap
             if (node.next != null) {
                 minHeap.offer(node.next);
             }
         }
 
+        // Step 5: Return the merged list starting from the next node of the dummy node
         return dummy.next;
     }
 }
