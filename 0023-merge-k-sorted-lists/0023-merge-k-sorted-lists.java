e4b3d136321c1d5cd4cdcf09530a091d16b224de
Shivanshu Sanjeev <55157249+Shivanshwho@users.noreply.github.com>
2023-09-16 12:11:31 +0530

    Time: 4 ms (74.98%), Space: 42.9 MB (96.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -35,3 +35,15 @@ class Solution {
         return dummy.next;
     }
 }
+
+// Explanation:
+
+// We create a priority queue (min-heap) to keep track of the smallest node from each list at any given time. The custom comparator ensures that the nodes are ordered by their values in ascending order.
+
+// We iterate through all the lists and add the first node from each list to the min-heap if it's not null. This initializes the min-heap with the initial elements.
+
+// We create a dummy node to simplify the code. The current pointer is used to keep track of the last node in the merged list.
+
+// We repeatedly extract the node with the smallest value from the min-heap and add it to the merged list. If this node has a next node in its original list, we add that next node to the min-heap for further processing.
+
+// Finally, we return the merged list starting from the next node of the dummy node, effectively excluding the dummy node from the result.
